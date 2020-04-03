18c2adbedc3b1953d284c0854172c431b5c0ee16
Nielsen Puma <nielhpuma@gmail.com>
2020-04-03 11:57:56 -0500

    BBDD POR SERVICIO de MSUSUARIOS
    se desvincula completamente de la base de datos MySQL para pasar a
    PostgreSQL según el patrón planteado.

--- microservicio-usuarios/src/main/java/com/nhpuma/app/usuarios/services/AlumnoService.java
+++ microservicio-usuarios/src/main/java/com/nhpuma/app/usuarios/services/AlumnoService.java
@@ -2,10 +2,16 @@ package com.nhpuma.app.usuarios.services;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.nhpuma.microservicios.commons.services.CommonService;
 import com.nhpuma.microservicios.commonsalumnos.models.entity.Alumno;
 
 public interface AlumnoService extends CommonService<Alumno> {
 
 	public List<Alumno> findByNombreOrApellido(String term);
+	
+	public Iterable<Alumno> findAllById(Iterable<Long> ids);
+	
+	public void eliminarCursoAlumnoPorId(Long id);
 }
